e01fbe5fe8e788ffbe8253601cfb6c7f62bc0d6f
Hirdesh Mewada <djhd221@gmail.com>
2024-09-07 12:17:05 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
+
 const app=express()
 
 app.use(cors({
@@ -17,6 +18,17 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))//url
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+// https://localhost:8000/api/v1/users/register
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+
+
 
 
 export { app }
